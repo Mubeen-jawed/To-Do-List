f0fcb82c49b3a97f8364ed2241158032acbc7b97
Mubeen jawed <mubeenjawed3@gmail.com>
2023-06-20 17:35:14 +0500

    add authentication

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 // Data adding to DB
-
-mongoose.connect('mongodb+srv://jawedmubeen905:test123@cluster0.cpwomdl.mongodb.net/todolistDB',
+//mongodb+srv://jawedmubeen905:test123@cluster0.cpwomdl.mongodb.net
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB',
   {
     useNewUrlParser: true
   }
@@ -52,32 +52,54 @@ const listSchema = new mongoose.Schema({
 
 const List = mongoose.model("List", listSchema);
 
+// signup/login user DB
+
+const usersSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String
+})
+
+const User = mongoose.model("User", usersSchema)
 
 // Routes
 
+let authenticated;
+
+// console.log(usersSchema.email, "email");
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
+app.get(`/`, function (req, res) {
+
+  // app.get('/:list', function (req, res) {
+  //   const customListItem = _.capitalize(req.params.list);
 
 
-  Item.find().exec()
-    .then((result) => {
+  if (authenticated) {
 
-      if (result.length === 0) {
-        Item.insertMany(defaultItems)
-      }
+    Item.find().exec()
+      .then((result) => {
 
-      else {
-        res.render("main/list", { listTitle: 'Today', newListItems: result })
-      }
+        if (result.length === 0) {
+          Item.insertMany(defaultItems)
+        }
 
-      res.redirect('/');
+        else {
+          res.render("main/list", { listTitle: 'Today', newListItems: result })
+        }
 
+        res.redirect('/');
 
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  else {
+    res.redirect('/signup')
+  }
 
 })
 
@@ -145,45 +167,124 @@ app.post('/delete', function (req, res) {
 
 })
 
-app.get('/:list', function (req, res) {
-  const customListItem = _.capitalize(req.params.list);
+// app.get('/:list', function (req, res) {
+//   const customListItem = _.capitalize(req.params.list);
 
-  List.findOne({ name: customListItem })
-    .then(function (foundList) {  // Success
+//   List.findOne({ name: customListItem })
+//     .then(function (foundList) {  // Success
 
-      // console.log(foundList.name);
+//       // console.log(foundList.name);
 
-      if (!foundList) {
+//       if (!foundList) {
 
-        // Create a new list
-        const list = new List({
-          name: customListItem,
-          items: defaultItems
-        })
+//         // Create a new list
+//         const list = new List({
+//           name: customListItem,
+//           items: defaultItems
+//         })
 
-        list.save();
+//         list.save();
 
-        res.redirect('/' + customListItem)
-      }
+//         res.redirect('/' + customListItem)
+//       }
 
-      else if (foundList) {
+//       else if (foundList) {
 
-        // Show a existing list
-        res.render('main/list', { listTitle: foundList.name, newListItems: foundList.items })
-      }
+//         // Show a existing list
+//         res.render('main/list', { listTitle: foundList.name, newListItems: foundList.items })
+//       }
 
-    }).catch(function (error) {
-      console.log(error); // Failure
-    });
+//     }).catch(function (error) {
+//       console.log(error); // Failure
+//     });
+
+// })
+
+
+app.get('/signup', function (req, res) {
+
+  if (!authenticated) {
+    res.render("main/signup")
+  }
+
+  else {
+    res.redirect("/")
+  }
 
 })
+
+app.post('/signup', function (req, res) {
+  let userFirstName = req.body.firstName;
+  let userLastName = req.body.lastName;
+  let userEmail = req.body.email;
+  let userPassword = req.body.password;
+
+  const userData = new User({
+    firstName: userFirstName,
+    lastName: userLastName,
+    email: userEmail,
+    password: userPassword,
+  })
+  if (userFirstName.length && userLastName.length && userEmail.length && userPassword.length != 0) {
+    authenticated = true
+    res.redirect("/")
+    console.log("Fill up all the fields");
+  }
+
+  else {
+    res.redirect("/signup")
+  }
+
+
+  userData.save()
+
+})
+
+app.get("/login", function (req, res) {
+  if (!authenticated) {
+    res.render("main/login")
+  }
+
+  else {
+    res.redirect("/")
+  }
+})
+
+app.post("/login", function (req, res) {
+
+  let loginEmail = req.body.loginEmail
+  let loginPassword = req.body.loginPassword
+
+  if (loginEmail.length && loginPassword.length != 0) {
+
+    User.findOne({ email: loginEmail, password: loginPassword })
+      .then((foundList) => {
+        if (foundList) {
+          res.redirect('/')
+          authenticated = true
+        } else {
+          res.redirect("/login")
+          console.log("user not exist");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  } else {
+    res.redirect("/login")
+    console.log("Fill up all the fields");
+  }
+})
+
+// app.post("/logout", function (req, res) {
+//   authenticated = false
+// })
+
 
 
 app.get("/about", function (req, res) {
   res.render("main/about")
 })
-
-
 
 app.listen('3000', function () {
   console.log("Server has started on port 3000");
